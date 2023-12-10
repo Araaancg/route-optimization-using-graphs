69cd0deb9bab2392a5e6ce86e285fc427904547f
unknown <carpinteroaranchag@gmail.com>
2023-12-10 13:59:10 +0100

    feat: createGraph function

---
+++ route-optimization-using-graphs.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+const int MAX_CITIES = 10;  // to determine size of vector
+
+// function to create an adjacency matrix for the given cities and distances
+vector<vector<int>> createGraph() {
+    // map city names to unique indices
+    unordered_map<string, int> cityIndex;
+
+    vector<string> cities = { "Madrid", "Guadalajara", "Toledo", "Caceres", "Ciudad Real", "Jaen", "Salamanca", "Albacete" };
+
+    // initialize the adjacency matrix with all distances set to 0 -> no connection between cities
+    vector<vector<int>> graph(MAX_CITIES, vector<int>(MAX_CITIES));
+
+    // Assign unique indices to cities
+    for (int i = 0; i < cities.size(); ++i) {
+        cityIndex[cities[i]] = i;
+    }
+
+    // Add distances to the adjacency matrix
+    graph[cityIndex["Madrid"]][cityIndex["Ciudad Real"]] = 100;
+    graph[cityIndex["Ciudad Real"]][cityIndex["Madrid"]] = 100;
+
+    graph[cityIndex["Ciudad Real"]][cityIndex["Salamanca"]] = 100;
+    graph[cityIndex["Salamanca"]][cityIndex["Ciudad Real"]] = 100;
+
+    graph[cityIndex["Madrid"]][cityIndex["Toledo"]] = 80;
+    graph[cityIndex["Toledo"]][cityIndex["Madrid"]] = 80;
+
+    graph[cityIndex["Madrid"]][cityIndex["Salamanca"]] = 190;
+    graph[cityIndex["Salamanca"]][cityIndex["Madrid"]] = 190;
+
+    graph[cityIndex["Toledo"]][cityIndex["Albacete"]] = 150;
+    graph[cityIndex["Albacete"]][cityIndex["Toledo"]] = 150;
+
+    graph[cityIndex["Toledo"]][cityIndex["Jaén"]] = 180;
+    graph[cityIndex["Jaén"]][cityIndex["Toledo"]] = 180;
+
+    graph[cityIndex["Toledo"]][cityIndex["Ciudad Real"]] = 50;
+    graph[cityIndex["Ciudad Real"]][cityIndex["Toledo"]] = 50;
+
+    graph[cityIndex["Salamanca"]][cityIndex["Guadalajara"]] = 100;
+    graph[cityIndex["Guadalajara"]][cityIndex["Salamanca"]] = 100;
+
+    graph[cityIndex["Salamanca"]][cityIndex["Caceres"]] = 150;
+    graph[cityIndex["Caceres"]][cityIndex["Salamanca"]] = 150;
+
+    graph[cityIndex["Caceres"]][cityIndex["Jaen"]] = 150;
+    graph[cityIndex["Jaén"]][cityIndex["Caceres"]] = 150;
+
+    graph[cityIndex["Salamanca"]][cityIndex["Jaen"]] = 100;
+    graph[cityIndex["Jaen"]][cityIndex["Salamanca"]] = 100;
+
+    return graph;
+}
+
+int main() {
+    vector<vector<int>> adjacencyMatrix = createGraph();
+
+    // Display the adjacency matrix
+    cout << "Adjacency Matrix:" << endl;
+    for (const auto& row : adjacencyMatrix) {
+        for (int distance : row) {
+            cout << distance << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
