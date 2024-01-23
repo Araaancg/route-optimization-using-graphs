0d953fcb90c1d623caaf67e21b574baf695adf2b
Arancha Carpintero Guardiola <carpinteroaranchag@gmail.com>
2024-01-23 15:19:19 +0100

    Reapply "wip: dfs"
    This reverts commit e662ff60d179265408cc9da57dbfd1bc8bf594fd.

---
+++ dfs.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+
+
+using namespace std;
+
+vector<string> DFS(const vector<vector<int>>& graph, const string& origin, const string& destination) {
+
+    unordered_set<string> visitedNodes;
+    stack<string> myStack;
+    vector<string> path;
+
+    return {};
+}
